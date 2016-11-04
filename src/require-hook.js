30ebf66bd19a57b63ba2994706875c10dc21c72f
Ben Gotow <bengotow@gmail.com>
2016-11-04 14:33:08 -0700

    Only require-hook compilers that return a NodeJS-compatible type

--- src/require-hook.js
+++ src/require-hook.js
@@ -1,5 +1,10 @@
 import mimeTypes from './mime-types';
 
+const requirableMimeTypes = [
+  'application/javascript',
+  'application/json',
+];
+
 /**
  * Initializes the node.js hook that allows us to intercept files loaded by
  * node.js and rewrite them. This method along with {@link initializeProtocolHook}
@@ -9,8 +14,18 @@ import mimeTypes from './mime-types';
  * @param  {CompilerHost} compilerHost  The compiler host to use for compilation.
  */
 export default function registerRequireExtension(compilerHost) {
-  Object.keys(compilerHost.compilersByMimeType).forEach((mimeType) => {
-    mimeTypes.extensions(mimeType).forEach((ext) => {
+  Object.keys(compilerHost.compilersByMimeType).forEach((inputMimeType) => {
+    const compiler = compilerHost.compilersByMimeType[inputMimeType];
+    const outputMimeType = compiler.constructor.getOutputMimeType();
+
+    // Only expose extensions to NodeJS if the output of the compiler is
+    // supported by NodeJS. This prevents module.resolve from returning
+    // `index.less`, for example, and trying to load LESS as JavaScript.
+    if (outputMimeType && !requirableMimeTypes.includes(outputMimeType)) {
+      return;
+    }
+
+    mimeTypes.extensions(inputMimeType).forEach((ext) => {
       require.extensions[`.${ext}`] = (module, filename) => {
         let {code} = compilerHost.compileSync(filename);
         module._compile(code, filename);
